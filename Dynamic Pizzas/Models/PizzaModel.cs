dccb3faf21b91603e30fa21f22b79ae381104ee5
Broscan <Calle992015@gmail.com>
2024-02-19 16:06:15 +0100

    initial

---
+++ Dynamic Pizzas/Models/PizzaModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Dynamic_Pizzas.Models
+{
+	public class PizzaModel
+	{
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public string? Description { get; set; }
+		public int Deliciousness { get; set; }
+		public string? ImageUrl { get; set; }
+	}
+
+}
